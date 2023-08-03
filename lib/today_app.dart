9a527f89bb81ad9204aa84e266930f5642b2f20c
daniilbelikov <pvt.belikov@gmail.com>
2023-08-03 23:59:41 +0400

    Created project

---
+++ lib/today_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodayApp extends StatelessWidget {
+  const TodayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
